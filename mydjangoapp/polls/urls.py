ddd007a588477e4d5f87ecc30f74925c3602e839
amy wright <axil.wright@gmail.com>
2023-02-01 17:11:28 +0100

    add changes

--- mydjangoapp/polls/urls.py
+++ mydjangoapp/polls/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.index,name='indexy')
+    path('',views.index,name='indexy'),
+   
+    path('hello/<str:name>/<int:age>/',views.hello,name="hello"),
+     path('<str:name>/',views.greet,name="greet"),
 ]
--- mydjangoapp/polls/views.py
+++ mydjangoapp/polls/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World")
+def greet(request,name):
+    return HttpResponse(f"Hello your name is : {name}")
+def hello(request,name,age):
+    return render(request,"polls/index.html",{'name':name,'age':age})
