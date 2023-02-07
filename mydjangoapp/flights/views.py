7d09393ebf8d9f715779d5623f7cf0ca8e445895
amy wright <axil.wright@gmail.com>
2023-02-07 21:33:39 +0100

    add new files

--- mydjangoapp/flights/views.py
+++ mydjangoapp/flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import flight
 # Create your views here.
-def flights(request):
-    return HttpResponse('hiiii')
+def index(request):
+  return render(request,"flights/index.html",{
+    'Flights' : flight.objects.all()
+  })
