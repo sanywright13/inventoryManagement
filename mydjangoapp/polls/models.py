15082f598b963e3a8327ab526b05c84f28fa3e3f
amy wright <axil.wright@gmail.com>
2023-02-10 20:17:49 +0100

    commit new changes

--- mydjangoapp/polls/models.py
+++ mydjangoapp/polls/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Question(models.Model):
     question_text=models.CharField(max_length=63)
     pub_date=models.DateTimeField()
+
+    '''
     def __str__(self):
         return f' question : {self.question_text} published the : {self.pub_date}'
+    '''
+
 class Choice1(models.Model):
-    question=models.ForeignKey(Question,on_delete=models.CASCADE,related_name="proposition")  
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)  
     choice_text=models.CharField(max_length=200,null=0, blank=0,)
     votes=models.IntegerField(default=0)      
--- mydjangoapp/polls/views.py
+++ mydjangoapp/polls/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
 from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("Hello World")
-def greet(request,name):
-    return HttpResponse(f"Hello your name is : {name}")
-def hello(request,name,age):
-    return render(request,"polls/index.html",{
-        'name':name,'age':age})
+    question=Question.objects.order_by('pub_date')
+    output=[q for q in question]
+    return render(request,'polls/index.html',{
+        'output' : output
+    })
+def detail(request,question_id):
+    #get the question of question_id
+    question=Question.objects.get(pk=question_id)
+    print(question.id)
+    try:
+        return render(request,'polls/detail.html',{
+            'question':question
+        })
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return HttpResponse(f'you are looking to question {question_id}')
+def results(request,question_id):
+    return  HttpResponse(f'this is results of auestion ID :{question_id}')
+def vote(request,question_id):
+    return HttpResponse(f' numbers of vote for question ID :{question_id}')
