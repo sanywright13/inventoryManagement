25be40c7213b54e04e218b2f3e69af7e79fe95bc
sanywright13 <axil.wright@gmail.com>
2023-08-11 12:20:50 +0100

    New commit message

--- clemin/urls.py
+++ clemin/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+<<<<<<< HEAD
 from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/',include("iventory.urls"))
+=======
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+>>>>>>> 73ae60d (configure postgress database with django)
 ]
